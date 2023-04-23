781341fdedf911e659dabc7409c3c898bd8d0ba2
Ajay <ajka9053@colorado.edu>
2023-04-23 09:30:42 +0000

    Initial commit for Final Project * Changed defconfig to raspberrypi3_defconfig * Added ncurses and dropbear support in buildroot * Git repo path set to final-project-application in aesd-assignments.mk * Tested image on Raspberry Pi and tested single-player ping-pong game

--- runqemu.sh
+++ runqemu.sh
@@ -4,12 +4,21 @@
 #Author: Siddhant Jajoo.
 
 
+# qemu-system-aarch64 \
+#     -M raspi3  \
+#     -cpu cortex-a72 -nographic -smp 4 \
+#     -kernel buildroot/output/images/sdcard.img \
+#     -append "rootwait root=/dev/vda console=ttyAMA0" \
+#     -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
+#     -device virtio-net-device,netdev=eth0 \
+#     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
+#     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+
 qemu-system-aarch64 \
-    -M virt  \
-    -cpu cortex-a53 -nographic -smp 1 \
-    -kernel buildroot/output/images/Image \
-    -append "rootwait root=/dev/vda console=ttyAMA0" \
-    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
-    -device virtio-net-device,netdev=eth0 \
-    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
-    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+   -machine raspi3 \
+   -kernel buildroot/output/images/zImage \
+   -sd buildroot/output/images/sdcard.img \
+   -dtb buildroot/output/images/bcm2710-rpi-3-b-plus.dtb \
+   -nographic -m 1024 \
+#    -append "rw console=ttyAMA0,115200 root=/dev/mmcblk0 fsck.repair=yes rootwait" \
+#    -device sd-card,drive=mycard -drive if=none,file=buildroot/output/images/rootfs.ext4,format=raw,id=mycard
