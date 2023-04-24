0c3023eb5f73b863c03daf040b5044142225f3d3
Ajay <ajka9053@colorado.edu>
2023-04-24 00:20:21 +0000

    Changes for building pingpong game with libtcpipc.so library * Enabled openssh in menuconfig * Able to connect to RPi over ssh via ethernet * Multiplayer gaming capability tested: Server being RPi and Client being Ubuntu VM

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f4892858de4a593c69e43cc4be64452d66821668
+AESD_ASSIGNMENTS_VERSION = 88dcd4b7092d8934b8313c4bc5b7c9e2d4cd48ee
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,16 +15,14 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 # TODO add your aesdsocket application build steps below
-#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lib/libtcpipc all
-#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/app/ping-pong all
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lib/libtcpipc all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/app/ping-pong all
 endef
 
 # TODO add your aesdsocket application and startup script installation steps below
-#$(INSTALL) -m 0755 $(@D)/lib/libtcpipc/libtcpipc.so  $(TARGET_DIR)/usr/lib
-#$(INSTALL) -m 0755 $(@D)/app/ping-pong/pingpong $(TARGET_DIR)/usr/bin
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/lib/libtcpipc/libtcpipc.so  $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 0755 $(@D)/app/ping-pong/pingpong $(TARGET_DIR)/usr/bin
 endef
 
